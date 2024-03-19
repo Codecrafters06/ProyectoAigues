3c639b5342c434dfb2de321db5ac0e228fbde726
Inno Melkonjan <monkeytoffy@gmail.com>
2024-03-19 22:44:57 +0100

    done-with-fake-avatar-missing-calculator-and-incomplete-form

---
+++ client/src/features/pageHome/components/HomeButton.tsx
@@ -0,0 +1,10 @@
+
+function HomeButton() {
+  return (
+    <>
+        <button className=' border-transparent border-3 px-14 py-3 rounded bg-[#06B9D3]'>Inicio</button>
+    </>
+  )
+}
+
+export default HomeButton
