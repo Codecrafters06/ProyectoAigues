521785ebcc1eb77e7d12a2b0bc1ac6742a1c1ec9
sefogi <sebast18@gmail.com>
2024-03-19 17:16:43 +0100

    barraok 1.1

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: FC = () => {
   return (
     <>
     
-     <ul className="bg-blue-100 flex flex-row justify-around items-center w-full">
+     <ul className="bg-blue-100 flex flex-row justify-around items-center h-24 w-full">
      <li><img src="quiz.png"></img></li>
      <li><img src="ph_drop-light.png"></img></li>
      <li><img src="home.png"></img></li>
