836e4c929f6560c0bb28925bb0d4b4f29a402ad3
mava <mavafiza@gmail.com>
2024-03-20 03:09:52 +0100

    Creacion y estilos de FormTriviaAqua, FormTriviaDrGotas, FormTriviaEco, TriviaAqua1 y 2, TriviaEco1 y 2, TriviaGotas1 y 2 y PageTrivia

--- client/src/features/pageHome/Home.tsx
+++ client/src/features/pageHome/Home.tsx
@@ -6,8 +6,8 @@ import Hero from './components/Hero'
 const Home: FC = () => {
   return (
     <Layout>
-      <Hero/>
       <h1 className='font-bold text-center text-sky-700'>Home</h1>
+      <Hero/>
 
     </Layout>
 
--- client/src/features/pageTrivia/Trivia.tsx
+++ client/src/features/pageTrivia/Trivia.tsx
@@ -1,17 +1,24 @@
 import React, { FC } from 'react';
-import { Layout } from '../../components'
-import TriviaComponent from './components/TriviaComponent'
-
+import { Layout } from '../../components';
+import TriviaAqua1 from './components/TriviaAqua1';
+import TriviaAqua2 from './components/TriviaAqua2';
+import TriviaEco1 from './components/TriviaEco1';
+import TriviaEco2 from './components/TriviaEco2';
+import TriviaGotas1 from './components/TriviaGotas1';
+import TriviaGotas2 from './components/TriviaGotas2';
 
 const Trivia: FC = () => {
   return (
     <Layout>
-      <TriviaComponent/>
-      <h2 className='font-bold text-center text-sky-700'>Trivia</h2>
+      <TriviaAqua1 />
+      <TriviaAqua2 />
+      <TriviaEco1 />
+      <TriviaEco2 />
+      <TriviaGotas1 />
+      <TriviaGotas2 />
 
     </Layout>
-
-  )
+  );
 }
 
-export default Trivia
+export default Trivia;
