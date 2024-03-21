5a1c29abd9858fbb5de99df8359cb7994ddcc8e6
sefogi <sebast18@gmail.com>
2024-03-21 01:47:09 +0100

    actualizacion 1.1

--- client/src/features/pageInstructions/Instructions.tsx
+++ client/src/features/pageInstructions/Instructions.tsx
@@ -1,35 +1,33 @@
 import React, { FC } from 'react';
-import {Layout } from '../../components'
-import InstructionsComponent from './components/InstructionsComponent'
+import { Layout } from '../../components';
+import InstructionsComponent from './components/InstructionsComponent';
 
-
- const Instructions: FC = () => {
+const Instructions: FC = () => {
   return (
     <Layout>
-      <InstructionsComponent/>
-      <>
-      <h2 className='m-20 font-bold text-center text-sky-700'>Instruciones</h2>
-      <p className="text-center text-sm m-30 text-black-500 font-bold p-5 ">
-      Respondiendo preguntas sobre la preservación del agua:
-      <ul className='list-disc list-inside m-1 text-center'>
-      <li>Si respondes en un sólo intento ganas 2 gotas</li>
-      <li>Si respondes en el segundo intento ganas 1 gota</li>
-      <li>Si saltas la pregunta pierdes 1 gota</li>
-      </ul>
-      <p className='m-5'>Completa desafíos y actividades interactivas en tu propio hogar para ganar gotas extras</p>
-      <p className='m-5'>Si compartes tu experiencia en las redes sociales y promueves el Museo entre amigos y familiares ganaras gotas extras</p>
-      </p>
-      <button className=' h-12 absolute bottom-0 w-full flex justify-center items-center'>
-                <button className='h-8 w-auto p-1 bg-cyan-700 rounded-md'>Siguiente</button>
-      </button>
-      </>
-      
-      
-
-
+      <div className='flex items-center justify-center bg-bridge-image bg-cover h-screen w-full bg-no-repeat bg-center'>
+        <div className='text-center p-5'>
+          <p className="text-sm text-black font-bold mb-5">
+            Respondiendo preguntas sobre la preservación del agua:
+          </p>
+          <ul className='list-disc list-inside m-1'>
+            <li>Si respondes en un sólo intento ganas 2 gotas</li>
+            <li>Si respondes en el segundo intento ganas 1 gota</li>
+            <li>Si saltas la pregunta pierdes 1 gota</li>
+          </ul>
+          <p className='mt-5'>Completa desafíos y actividades interactivas en tu propio hogar para ganar gotas extras</p>
+          <p className='mt-5'>Si compartes tu experiencia en las redes sociales y promueves el Museo entre amigos y familiares ganaras gotas extras</p>
+          
+          {/* Botón centrado al final */}
+          <div className='mt-5 flex justify-center'>
+            <button className='h-8 w-auto px-4 py-1 bg-cyan-700 text-white rounded-md'>
+              Siguiente
+            </button>
+          </div>
+        </div>
+      </div>
     </Layout>
-
   )
 }
 
-export default Instructions
+export default Instructions;
