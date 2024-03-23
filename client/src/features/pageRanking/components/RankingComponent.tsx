98de95570f4f712432279def888959b074e05694
Inno Melkonjan <monkeytoffy@gmail.com>
2024-03-23 14:48:48 +0100

    tweaks

--- client/src/features/pageRanking/components/RankingComponent.tsx
+++ client/src/features/pageRanking/components/RankingComponent.tsx
@@ -1,18 +1,12 @@
 import React, { FC } from 'react';
-import Droplet from '../../../../../client/public/ph_drop-light.png'
 
 const RankingComponent: FC = () => {
+  
   return (
     <div className='flex items-center justify-around w-11/12'>
     <table className=' w-11/12'>
-      <tr className='flex justify-around w-11/12'>
-        <th className=' w-4/12'>rank</th>
-        <th className=' w-4/12'>avatar</th>
-        <th className=' w-4/12'>Usuario</th>
-        <th className=' w-4/12'>points</th>
-      </tr>
-      <tr className='flex w-11/12 justify-around'>
-        <td className=' h-8 flex justify-center items-center'>
+      <tr className='flex w-11/12 justify-around items-center'>
+        <td className=' h-10 flex justify-center items-center'>
         <div className=' flex justify-center items-end bg-[gold] shadow h-6 w-6 rounded-tl-0 rounded-tr-full rounded-bl-full rounded-br-full rotate-45 '>
           <div className=' -rotate-45'>1</div>
         </div>
