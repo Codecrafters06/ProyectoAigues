7d326f2740cfd54307c894c4223f95039bdf7b52
sefogi <sebast18@gmail.com>
2024-03-27 10:03:35 +0100

    upexample

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 ///<reference types="vitest"/>
 
-import { defineConfig } from 'vite'
+import {  defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+i
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
