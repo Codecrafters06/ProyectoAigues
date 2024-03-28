c90dd1964480d80e356d877defdf166ad5d7910e
wilpipe <residentsbcn@gmail.com>
2024-03-28 15:13:29 +0100

    last updated mixed with Isadora testing

--- server/src/usuarios/usuarios.controller.ts
+++ server/src/usuarios/usuarios.controller.ts
@@ -13,40 +13,143 @@
 // export default UsuariosController;
 
 
-import { Controller, Get, Post, Body, Param, Put, Delete, Res, HttpStatus } from '@nestjs/common';
+//---------------------------------------------------------------------------------------------------------------------
+// import { Controller, Get, Post, Body, Param, Put, Delete, Res, HttpStatus } from '@nestjs/common';
+// import { UsuariosService } from './usuarios.service';
+// import { Usuarios } from './schemas/usuarios.schema';
+
+// @Controller('usuarios')
+// export class UsuariosController {
+//   constructor(private readonly usuariosService: UsuariosService) {}
+
+//   @Get()
+//   findAll(@Res() response){
+//     const allUsers = this.usuariosService.findAll();
+//     response.status(HttpStatus.OK).json({
+//       message: 'All users', allUsers
+//     })
+//     return this.usuariosService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.usuariosService.findOne(id);
+//   }
+
+//   @Post()
+//   create(@Body() usuario: Usuarios) {
+//     return this.usuariosService.create(usuario);
+//   }
+
+//   @Put(':id')
+//   update(@Param('id') id: string, @Body() usuario: Usuarios) {
+//     return this.usuariosService.update(id, usuario);
+//   }
+
+//   @Delete(':id')
+//   delete(@Param('id') id: string) {
+//     return this.usuariosService.delete(id);
+//   }
+// }
+
+//________________________________________________________________________________________________________
+
+import { Controller, Get, Post, Body, Param, Put, Delete, Res, HttpStatus, NotFoundException } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { Usuarios } from './schemas/usuarios.schema';
-
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
-
   @Get()
-  findAll(@Res() response){
-    const allUsers = this.usuariosService.findAll();
-    response.status(HttpStatus.OK).json({
-      message: 'All users', allUsers
-    })
-    return this.usuariosService.findAll();
+  async findAll(@Res() response){
+    try {
+      const allUsers = await this.usuariosService.findAll();
+      response.status(HttpStatus.OK).json({
+        data: allUsers
+      });
+    } catch (error) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'Erro al obtener usuários',
+        error: error.message
+      });
+    }
   }
-
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usuariosService.findOne(id);
+  async findOne(@Param('id') id: string, @Res() response) {
+    try {
+      const user = await this.usuariosService.findOne(id);
+      response.status(HttpStatus.OK).json({
+        message: 'Usuário encontrado',
+        data: user
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        response.status(HttpStatus.NOT_FOUND).json({
+          message: error.message
+        });
+      } else {
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: 'Erro al obtener usuário',
+          error: error.message
+        });
+      }
+    }
   }
-
   @Post()
-  create(@Body() usuario: Usuarios) {
-    return this.usuariosService.create(usuario);
+  async create(@Body() usuario: Usuarios, @Res() response) {
+    try {
+      const newUser = await this.usuariosService.create(usuario);
+      response.status(HttpStatus.CREATED).json({
+        message: 'Usuário creado con exito',
+        data: newUser
+      });
+    } catch (error) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'Erro al crear usuário',
+        error: error.message
+      });
+    }
   }
-
   @Put(':id')
-  update(@Param('id') id: string, @Body() usuario: Usuarios) {
-    return this.usuariosService.update(id, usuario);
+  async update(@Param('id') id: string, @Body() usuario: Usuarios, @Res() response) {
+    try {
+      const updatedUser = await this.usuariosService.update(id, usuario);
+      response.status(HttpStatus.OK).json({
+        message: 'Usuário atualizado con exito',
+        data: updatedUser
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        response.status(HttpStatus.NOT_FOUND).json({
+          message: error.message
+        });
+      } else {
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: 'Erro al atualizar usuário',
+          error: error.message
+        });
+      }
+    }
   }
-
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.usuariosService.delete(id);
+  async delete(@Param('id') id: string, @Res() response) {
+    try {
+      const deletedUser = await this.usuariosService.delete(id);
+      response.status(HttpStatus.OK).json({
+        message: 'Usuário deletado con exito',
+        data: deletedUser
+      });
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        response.status(HttpStatus.NOT_FOUND).json({
+          message: error.message
+        });
+      } else {
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          message: 'Error al deletar usuário',
+          error: error.message
+        });
+      }
+    }
   }
 }
--- server/src/usuarios/usuarios.service.ts
+++ server/src/usuarios/usuarios.service.ts
@@ -1,32 +1,70 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
+// import { Injectable } from '@nestjs/common';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Model } from 'mongoose';
+// import { Usuarios, UsuariosDocument } from './schemas/usuarios.schema';
+
+// @Injectable()
+// export class UsuariosService {
+//   constructor(@InjectModel(Usuarios.name) private usuarioModel: Model<UsuariosDocument>) {}
+
+//   async findAll(): Promise<Usuarios[]> {
+//     const allUsers = await this.usuarioModel.find().exec();
+//     console.log(allUsers);
+//     return allUsers;
+//   }
+
+//   async findOne(id: string): Promise<Usuarios> {
+//     return this.usuarioModel.findOne({ id }).exec();
+//   }
+
+//   async create(usuario: Usuarios): Promise<Usuarios> {
+//     const newUsuario = new this.usuarioModel(usuario);
+//     return newUsuario.save();
+//   }
+
+//   async update(id: string, usuario: Usuarios): Promise<Usuarios> {
+//     return this.usuarioModel.findByIdAndUpdate(id, usuario, { new: true }).exec();
+//   }
+
+//   async delete(id: string): Promise<Usuarios> {
+//     return this.usuarioModel.findByIdAndDelete(id).exec();
+//   }
+// }
+
+
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Usuarios, UsuariosDocument } from './schemas/usuarios.schema';
-
+import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class UsuariosService {
   constructor(@InjectModel(Usuarios.name) private usuarioModel: Model<UsuariosDocument>) {}
-
   async findAll(): Promise<Usuarios[]> {
-    const allUsers = await this.usuarioModel.find().exec();
-    console.log(allUsers);
-    return allUsers;
+return this.usuarioModel.find().exec();
   }
-
   async findOne(id: string): Promise<Usuarios> {
-    return this.usuarioModel.findOne({ id }).exec();
+    const user = await this.usuarioModel.findById(id).exec();
+    if (!user) {
+      throw new NotFoundException('Usuário no encontrado');
+    }
+    return user;
   }
-
   async create(usuario: Usuarios): Promise<Usuarios> {
-    const newUsuario = new this.usuarioModel(usuario);
-    return newUsuario.save();
+    const newUser = new this.usuarioModel(usuario);
+    return newUser.save();
   }
-
   async update(id: string, usuario: Usuarios): Promise<Usuarios> {
-    return this.usuarioModel.findByIdAndUpdate(id, usuario, { new: true }).exec();
+    const updatedUser = await this.usuarioModel.findByIdAndUpdate(id, usuario, { new: true }).exec();
+    if (!updatedUser) {
+      throw new NotFoundException('Usuário no encontrado');
+    }
+    return updatedUser;
   }
-
   async delete(id: string): Promise<Usuarios> {
-    return this.usuarioModel.findByIdAndDelete(id).exec();
+    const deletedUser = await this.usuarioModel.findByIdAndDelete(id).exec();
+    if (!deletedUser) {
+      throw new NotFoundException('Usuário no encontrado');
+    }
+    return deletedUser;
   }
 }
