a0f859b29587a878849bd62e09deec88cc28e462
mava <mavafiza@gmail.com>
2024-03-29 00:49:26 +0100

    RegisterForm & loginForm working with zod validator correctly

--- client/src/features/pageLogin/components/LoginForm.tsx
+++ client/src/features/pageLogin/components/LoginForm.tsx
@@ -1,143 +1,36 @@
-// import React, { useState } from 'react';
-// import { useForm, Controller } from 'react-hook-form';
-// import { zodResolver } from '@hookform/resolvers/zod';
-// import { object, string } from 'zod';
-// import { Link } from 'react-router-dom'
-
-// const schema = object({
-//     name: string().nonempty({ message: 'El nombre es requerido' }).regex(/^[^\d]+$/, { message: 'El nombre no debe contener números' }),
-//     // age: number().nonnegative({ message: 'La edad debe ser un número positivo' }),
-//     email: string().email({ message: 'El email no es válido' }),
-//     password: string().min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
-// });
-
-// const LoginForm = () => {
-
-//     const [success, setSuccess] = useState(null);
-
-//     const { handleSubmit, control, formState: { errors }, reset } = useForm({
-
-
-//         resolver: zodResolver(schema)
-//     });
-
-//     const onSubmit = (data) => {
-//         console.log(data);
-
-//         reset({ name: '', email: '', password: '' });
-
-//         setSuccess('Application was submitted!');
-//         setTimeout(() => {
-//             setSuccess(null);
-//         }, 3000);
-//     };
-
-//     const errorMessages = Object.values(errors).map(error => error.message);
-
-//     const messageAnimation = {
-//         hidden: { y: 30, opacity: 0 },
-//         animate: { y: 0, opacity: 1, transition: { delay: 0.2, duration: 0.4 } },
-//     };
-
-//     const formFields = [
-//         { name: 'Usuario', type: 'text' },
-//         { name: 'email', type: 'text' },
-//         { name: 'contraseña', type: 'password' },
-//     ];
-
-//     return (
-
-//         <form className='h-72 w-60 bg-slate-400 bg-opacity-70 text-center flex flex-col relative pt-10 mt-10 mx-auto rounded-sm' onSubmit={handleSubmit(onSubmit)}>
-//             {formFields.map(({ name, type }, index) => (
-//                 <div className='flex flex-col p-2' key={index}>
-
-//                     <Controller
-//                         name={name}
-//                         control={control}
-//                         defaultValue={type === 'number' ? 0 : ''}
-//                         render={({ field }) => (
-
-//                             <input className='rounded-md leading-[14px] p-1 mx-3 h-auto bg-stone-50 opacity-65'
-//                                 {...field}
-//                                 type={type}
-//                                 placeholder={`Escribe tu ${name.toLocaleLowerCase()}`}
-//                                 value={field.value}
-//                                 onChange={(e) => field.onChange(type === 'number' ? parseInt(e.target.value, 10) : e.target.value)}
-//                             />
-//                         )}
-//                     />
-//                 </div>
-//             ))}
-
-//             <div className='flex flex-row justify-center gap-1'>
-//                 <input type="checkbox" className='border border-secondary' />
-//                 <p>Recuérdame</p>
-//             </div>
-
-//             <div className='flex flex-row justify-center p-8 gap-10'>
-
-//                 <button className="h-8 w-auto py-1 px-2 bg-cyan-700 rounded-md text-secondary cursor-pointer hover:bg-tertiary hover:text-primary">LogIn</button>
-//                <Link to="/register"> <button className="h-8 w-auto py-1 px-2 bg-cyan-700 rounded-md text-secondary cursor-pointer hover:bg-tertiary hover:text-primary">Register</button></Link>
-
-//             </div>
-
-//             {errorMessages.length > 0 && errorMessages.map((message, index) => (
-//                 <div key={index} variants={messageAnimation} initial="hidden" animate="animate" exit="hidden">{message}</div>
-//             ))}
-//             {success && <div variants={messageAnimation} initial="hidden" animate="animate" exit="hidden">{success}</div>}
-
-//         </form>
-//     );
-// };
-
-// export default LoginForm; 
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { object, string } from 'zod';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion';
 
 interface FormData {
-    name: string;
+    fieldName: string;
     email: string;
     password: string;
 }
 
-
-
 const schema = object({
-    name: string().nonempty({ message: 'El nombre es requerido' }).regex(/^[^\d]+$/, { message: 'El nombre no debe contener números' }),
-    // age: number().nonnegative({ message: 'La edad debe ser un número positivo' }),
+    fieldName: string().nonempty({ message: 'El nombre es requerido' }).regex(/^[A-Za-z]+$/, { message: 'El nombre no debe contener números' }),
     email: string().email({ message: 'El email no es válido' }),
     password: string().min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
 });
 
 const LoginForm = () => {
-
     const [success, setSuccess] = useState<string | null>(null);
-
-
-    const { handleSubmit, control, formState: { errors }, reset } = useForm({
-
-
+    const { handleSubmit, control, formState: { errors }, reset } = useForm<FormData>({
         resolver: zodResolver(schema)
     });
+    const navigate = useNavigate();
 
-    const onSubmit = (data: FormData) => {
+    const onSubmit: SubmitHandler<FormData> = (data) => {
         console.log(data);
-
-        reset({ name: '', email: '', password: '' });
-
-        setSuccess('Application was submitted!');
+        reset({ fieldName: '', email: '', password: '' });
+        setSuccess('¡ya vas a entrar!');
         setTimeout(() => {
             setSuccess(null);
+            navigate('/intro');
         }, 3000);
     };
 
@@ -149,30 +42,32 @@ const LoginForm = () => {
         animate: { y: 0, opacity: 1, transition: { delay: 0.2, duration: 0.4 } },
     };
 
-    const formFields = [
-        { name: 'Usuario', type: 'text' },
-        { name: 'email', type: 'text' },
-        { name: 'contraseña', type: 'password' },
+    type FieldName = "fieldName" | "email" | "password" ;
+
+    const formFields: { fieldName: FieldName, type: 'text' | 'password', placeholder: string }[] = [
+        { fieldName: 'fieldName', type: 'text', placeholder: 'Nombre'  },
+        { fieldName: 'email', type: 'text', placeholder: 'Correo electrónico' },
+        { fieldName: 'password', type: 'password', placeholder: 'Contraseña' },
     ];
 
     return (
 
         <form className='h-72 w-60 bg-slate-400 bg-opacity-70 text-center flex flex-col relative pt-10 mt-10 mx-auto rounded-sm' onSubmit={handleSubmit(onSubmit)}>
-            {formFields.map(({ name, type }, index) => (
+            {formFields.map(({ fieldName, type, placeholder }, index) => (
                 <div className='flex flex-col p-2' key={index}>
 
-                    <Controller
-                        name={name}
+                    <Controller<FormData>
+                        name={fieldName}
                         control={control}
-                        defaultValue={type === 'number' ? 0 : ''}
+                        defaultValue=''
                         render={({ field }) => (
 
                             <input className='rounded-md leading-[14px] p-1 mx-3 h-auto bg-stone-50 opacity-65'
                                 {...field}
                                 type={type}
-                                placeholder={`Escribe tu ${name.toLocaleLowerCase()}`}
+                                placeholder={placeholder}
                                 value={field.value}
-                                onChange={(e) => field.onChange(type === 'number' ? parseInt(e.target.value, 10) : e.target.value)}
+                                onChange={(e) => field.onChange(e.target.value)}
                             />
                         )}
                     />
@@ -187,17 +82,16 @@ const LoginForm = () => {
             <div className='flex flex-row justify-center p-8 gap-10'>
 
                 <button className="h-8 w-auto py-1 px-2 bg-cyan-700 rounded-md text-secondary cursor-pointer hover:bg-tertiary hover:text-primary">LogIn</button>
-               <Link to="/register"> <button className="h-8 w-auto py-1 px-2 bg-cyan-700 rounded-md text-secondary cursor-pointer hover:bg-tertiary hover:text-primary">Register</button></Link>
+                <Link to="/register"> <button className="h-8 w-auto py-1 px-2 bg-cyan-700 rounded-md text-secondary cursor-pointer hover:bg-tertiary hover:text-primary">Register</button></Link>
 
             </div>
 
             {errorMessages.length > 0 && errorMessages.map((message, index) => (
-                <div key={index} variants={messageAnimation} initial="hidden" animate="animate" exit="hidden">{message}</div>
+                <motion.div key={index} className="text-red-900 font-semibold w-60 bg-slate-400 bg-opacity-70 pb-0.5 rounded-sm" variants={messageAnimation} initial="hidden" animate="animate" exit="hidden">{message}</motion.div>
             ))}
-            {success && <div variants={messageAnimation} initial="hidden" animate="animate" exit="hidden">{success}</div>}
-
+            {success && <motion.div className="text-green-800 text-xl font-bold w-60 bg-slate-400 bg-opacity-70 pb-0.5 rounded-sm" variants={messageAnimation} initial="hidden" animate="animate" exit="hidden">{success}</motion.div>}
         </form>
     );
 };
 
-export default LoginForm; 
+export default LoginForm;
