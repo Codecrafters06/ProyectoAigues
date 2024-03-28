76df383a7425633b843e471d95edcbe72636c302
wilpipe <residentsbcn@gmail.com>
2024-03-28 15:33:35 +0100

    USUARIO FUNCIONA git add .git add .!

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PreguntasModule } from './preguntas/preguntas.module'; // Fixed the file path for the import statement
 import { MongooseModule } from '@nestjs/mongoose';
 import { EscenariosModule } from './escenarios/escenarios.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [PreguntasModule, MongooseModule.forRoot('mongodb+srv://aiguesfactoria:gntobYwYmvM2ISYe@cluster0.tvvzq92.mongodb.net/'), EscenariosModule],
+  imports: [PreguntasModule, MongooseModule.forRoot('mongodb+srv://aiguesfactoria:gntobYwYmvM2ISYe@cluster0.tvvzq92.mongodb.net/'), EscenariosModule, UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
