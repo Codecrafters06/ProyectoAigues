75c5e21d6109e67e1a16965a955814979a00c371
sefogi <sebast18@gmail.com>
2024-04-03 15:20:10 +0200

    pwa funcional

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,45 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-    VitePWA({ registerType: 'autoUpdate' })
+
+    VitePWA({
+      manifest:{
+        display:"standalone",
+        display_override:['window-controls-overlay',"minimal-ui"],
+        lang: 'es-ES',
+        name: 'Museo Aigues de Barcelona',
+        short_name:'Museo de las Aigues',
+        description:'Aplicacion web',
+        theme_color:'#85A7C6',
+        background_color:'#06B9D3',
+        icons:[
+          {
+            src:'pwa-64x64.png',
+            sizes:'64x64',
+            type:'image/png'
+          },
+          {
+            src:'pwa-192x192.png',
+            sizes:'192x192',
+            type:'image/png',
+            purpose:'any',
+
+          },
+          {
+            src:'pwa-512x512.png',
+            sizes:'512x512',
+            type:'image/png',
+            purpose:'maskable'
+          }
+
+
+        ]
+
+
+
+
+      }
+     })
   
   ],
 })
